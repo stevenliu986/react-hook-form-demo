1aad5b5d60f33d7fff4e7040932110d37c871fa6
StevenLiu <stevenliu986@gmail.com>
2023-12-30 14:07:09 +1100

    更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
-import { BasicForm, RegisterFields } from "./forms/index";
+import {
+  ApplyValidation,
+  BasicForm,
+  RegisterFields,
+  IntegratedWithExistingForm,
+} from "./forms/index";
 
 function App() {
   return (
     <>
       <BasicForm />
+      <br />
       <RegisterFields />
+      <br />
+      <ApplyValidation />
+      <br />
+      <IntegratedWithExistingForm />
+      <br />
     </>
   );
 }
--- src/forms/index.js
+++ src/forms/index.js
@@ -1,2 +1,4 @@
 export { BasicForm } from "./BasicForm";
 export { RegisterFields } from "./RegisterFields";
+export { ApplyValidation } from "./ApplyValidation";
+export { IntegratedWithExistingForm } from "./IntegratedWithExistingForm";
