d35547660de90a8a53dcd3888aec35004cd0472f
StevenLiu <stevenliu986@gmail.com>
2023-12-30 12:19:06 +1100

    添加注释

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,11 @@ function App() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      {/* defaultValue表示表单的默认值 */}
       <input defaultValue="test" {...register("example")} />
+      {/* register的第二个参数required表示该项在提交时不能为空 */}
       <input {...register("exampleRequired", { required: true })} />
+      {/* 这一行会将表单验证时定义的错误信息展示出来 */}
       {errors.exampleRequired && <span>This field is required</span>}
       <input type="submit" />
     </form>
