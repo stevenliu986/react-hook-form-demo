41a421f873e6bb9d51aa4fc9732280bfada91dbe
StevenLiu <stevenliu986@gmail.com>
2023-12-30 11:20:06 +1100

    basic usage of useForm

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
       <input defaultValue="test" {...register("example")} />
       <input {...register("exampleRequired", { required: true })} />
       {errors.exampleRequired && <span>This field is required</span>}
+      <input type="submit" />
     </form>
   );
 }
